933d70151c718d9069994b63a095cfe993952327
Brian Cobb <xlilcasper@gmail.com>
2011-06-25 20:37:40 -0400

    Added in AddNode/RemoveNode methods

--- src/com/oberonserver/timerank/Abilities.java
+++ src/com/oberonserver/timerank/Abilities.java
@@ -1,8 +1,16 @@
 package com.oberonserver.timerank;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+import com.oberonserver.perms.PermMethod;
+
 public class Abilities {
 	public String permission="timerank.ab";
 	public String desc="";
+	public String world="*";
 	public long time=-1;
 	public int cost=-1;	
 	public long minTime=-1;
@@ -18,5 +26,35 @@ public class Abilities {
 	public String rentLostMsg="";
 	public String rentGainedMsg="";
 	public boolean rentBroadcast=false;
+	
+	public List<String> Nodes=new LinkedList<String>();
+	
+	public boolean AddNodes(PermMethod perm,Player p)
+	{
+		boolean worked=true;
+		for(String node : Nodes)
+		{
+			//make sure all of them worked. If one returns fallse, worked will be false
+			if (worked) 
+				worked=perm.AddNode(p, node, world);
+			else
+				perm.AddNode(p, node, world);
+		}
+		return worked;
+	}
+	
+	public boolean RemoveNodes(PermMethod perm,Player p)
+	{
+		boolean worked=true;
+		for(String node : Nodes)
+		{
+			//make sure all of them worked. If one returns fallse, worked will be false
+			if (worked) 
+				worked=perm.RemoveNode(p, node, world);
+			else
+				perm.RemoveNode(p, node, world);
+		}
+		return worked;
+	}
 
 }
