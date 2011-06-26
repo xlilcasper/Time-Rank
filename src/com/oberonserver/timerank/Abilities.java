6a139aa4398727982dc2926823bd85faf41ff8e0
Brian Cobb <xlilcasper@gmail.com>
2011-06-25 20:20:02 -0400

    Added abilities class to hold permission node data

---
+++ src/com/oberonserver/timerank/Abilities.java
@@ -0,0 +1,22 @@
+package com.oberonserver.timerank;
+
+public class Abilities {
+	public String permission="timerank.ab";
+	public String desc="";
+	public long time=-1;
+	public int cost=-1;	
+	public long minTime=-1;
+	public double amount=1;
+	public String name="";	
+	public String msg="";
+	public boolean broadcast=false;
+	
+	public int rentCost=-1;
+	public double rentAmount=-1;
+	public long rentTime=-1;
+	public long rentMinTime=-1;
+	public String rentLostMsg="";
+	public String rentGainedMsg="";
+	public boolean rentBroadcast=false;
+
+}
